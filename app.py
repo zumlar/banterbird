bd847a3b214ad65f3d3eceee838920e17c08a093
zumlar <143119610+zumlar@users.noreply.github.com>
2025-04-22 13:32:44 -0500

    data,json now updates, Post button works

--- app.py
+++ app.py
@@ -13,14 +13,17 @@ def get_posts():
         posts = json.load(file)
     return jsonify(posts)
     
-@app.route('/api/posts', methods=['POST'])
+@app.route('/api/add_post', methods=['POST'])
 def add_post():
     new_post = request.get_json()
+
     with (open("data.json", "r")) as file:
         posts = json.load(file)
         posts.insert(0, new_post) # add the post at the top of the file
+
     with (open("data.json", "w")) as file:
         json.dump(posts, file, indent=4)
+
     return jsonify({"status": "success"}), 201
     
 
--- static/js/app.js
+++ static/js/app.js
@@ -11,8 +11,22 @@ function renderPost(post) {
 
 function submitPost() {
   const message = document.getElementById("postInput").value;
-  console.log("Would post:", message);
-  alert("Tweet submitted (not really yet)");
+  try{
+const response = fetch("/api/add_post",{
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    username, message
+  })  
+})
+  } catch(error)
+  {
+    console.log("Post failed 🤣", error)
+  } 
+
+
 }
 
 window.onload = async () => {

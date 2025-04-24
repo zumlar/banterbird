8f2c41ac609cdb0780ea039610820c107b295bc3
zumlar <143119610+zumlar@users.noreply.github.com>
2025-04-24 10:39:37 -0500

    Now no longer needs refreshes to see new posts

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,26 @@
 const username = "admin";
 
-function renderPost(post) {
+function renderPost(post, isNew = false) {
   const template = document
     .getElementById("post-template")
     .content.cloneNode(true);
   template.querySelector(".username").innerText = post.username;
   template.querySelector(".message").innerText = post.message;
   document.getElementById("feed").appendChild(template);
+
+  if (isNew){
+    document.getElementById("feed").prepend(template);
+  }else{
+    document.getElementById("feed").appendChild(template);
+  }
+
 }
+
 
 function submitPost() {
   const message = document.getElementById("postInput").value;
   try{
-const response = fetch("/api/add_post",{
+const response = await fetch("/api/add_post",{
   method: "POST",
   headers: {
     "Content-Type": "application/json",
@@ -20,7 +28,11 @@ const response = fetch("/api/add_post",{
   body: JSON.stringify({
     username, message
   })  
-})
+  });
+  if (response.ok){
+    renderPost({username,message},True);//Pass isNew as True'
+    document.getElementById("postInput)".value) =""; //Clear the input box
+  }
   } catch(error)
   {
     console.log("Post failed 🤣", error)

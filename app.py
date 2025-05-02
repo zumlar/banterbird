851fb0a6f94e6e25fd62592b84012f4d093271b6
zumlar <143119610+zumlar@users.noreply.github.com>
2025-05-02 10:28:50 -0500

    Updated to the Github Version
    Added CSS

--- app.py
+++ app.py
@@ -13,23 +13,22 @@ def login():
 
 @app.route('/api/posts')
 def get_posts():
-    with (open("data.json", "r")) as file:
+    with open('posts.json', 'r') as file:
         posts = json.load(file)
     return jsonify(posts)
-    
+
 @app.route('/api/add_post', methods=['POST'])
 def add_post():
     new_post = request.get_json()
-
-    with (open("data.json", "r")) as file:
-        posts = json.load(file)
-        posts.insert(0, new_post) # add the post at the top of the file
-
-    with (open("data.json", "w")) as file:
-        json.dump(posts, file, indent=4)
-
-    return jsonify({"status": "success"}), 201
     
+    with open('posts.json', 'r') as file:
+        posts = json.load(file)
+    posts.insert(0, new_post)
+    
+    with open('posts.json', 'w') as file:
+        json.dump(posts, file, indent=4)
+        
+    return jsonify({"status": "success"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,46 +1,41 @@
 let username = localStorage.getItem("username");
-if (!username){
+if (!username) {
   window.location.href = "/login";
 }
 
 function renderPost(post, isNew = false) {
   const template = document
-    .getElementById("post-template").content.cloneNode(true);
+    .getElementById("post-template")
+    .content.cloneNode(true);
   template.querySelector(".username").innerText = post.username;
   template.querySelector(".message").innerText = post.message;
-  document.getElementById("feed").appendChild(template);
-
-  if (isNew){
+  if (isNew) {
     document.getElementById("feed").prepend(template);
-  }else{
+  } else {
     document.getElementById("feed").appendChild(template);
   }
-
 }
-
 
 async function submitPost() {
   const message = document.getElementById("postInput").value;
-  try{
-const response = await fetch("/api/add_post",{
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    username, message
-  })  
-  });
-  if (response.ok){
-    renderPost({username,message},true);//Pass isNew as True'
-    document.getElementById("postInput").value =""; //Clear the input box
+  try {
+    const response = await fetch("/api/add_post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        message: message,
+      }),
+    });
+    if (response.ok) {
+      renderPost({ username: username, message: message }, true);
+      document.getElementById("postInput").value = ""; // Clear the input field for your tweet storm!
+    }
+  } catch (error) {
+    console.error("Error submitting post:", error);
   }
-  } catch(error)
-  {
-    console.log("Post failed 🤣", error)
-  } 
-
-
 }
 
 window.onload = async () => {
